0ffe3128a496e561b29ad0abc218d149eca5e528
alexhollinghead <alexhollinghead@gmail.com>
2022-03-16 11:07:18 -0400

    Added April Fool's bit

--- dailygarf.js
+++ dailygarf.js
@@ -6,8 +6,8 @@ const today = new Date();
 
 client.login(config.token).then(() => {
 
-    if(today.getDate() == 12 && today.getMonth() == 2) {
-        client.channels.get('422134197868691458').send('https://assets.amuniversal.com/b5bdb4206e700139533d005056a9545d.jpg').destroy();
+    if(today.getDate() == 1 && today.getMonth() == 3) {
+        client.channels.get('419250097373839360').send('https://assets.amuniversal.com/b5bdb4206e700139533d005056a9545d.jpg').destroy();
     }
     else{
         client.channels.get('419250097373839360').send({files: [garf.latest()]}).destroy();
